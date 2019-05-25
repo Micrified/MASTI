b14dda26df54a2d4e596ca5890f5254dc5f89b71
Barnabas Busa <bbusa@Barnabass-MacBook-Pro.local>
2019-05-25 15:56:40 +0200

    removing large file

---
+++ DNSSEC/dnssec.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+inputDomainList=domainListTestSmall.tsv
+
+for i in `seq 1 5`;
+do
+## this puts all the searched domains from the inputted domain list
+tld=$(sed "${i}q;d" ../tld.txt)
+cat $inputDomainList | grep .$tld. >> TLDSep/dot$tld.txt
+touch Results/resultdot$tld.txt
+done
+
+
+## This funciton gets the IPs from the dns_resolvers
+while read line; do
+  ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$line")"
+  echo "$ip" >> ../dns_resolversIP.txt
+done < "../dns_resolvers.txt"
+
+## Here you can select how many of the selected resolvers you want to be using
+for i in `seq 1 2`;
+do 
+    resolver=$(sed "${i}q;d" ../dns_resolversIP.txt)
+    for FILE in Results/*.txt
+    do  
+        echo Resolver: $resolver >> ${FILE}
+    done
+    ##This is the acutal line that returns how many ms it takes to find a query (only works if it has dnssec flag which is "ad")
+    for i in `seq 1 5`;
+    do
+        ## this puts all the searched domains from the inputted domain list
+        tld=$(sed "${i}q;d" ../tld.txt)
+        while read line; do 
+            dig @$resolver $line + dnssec +multi  | pcregrep -M "flags:(.*)ad(.|\n)*Query time:.*" | grep time | grep -Eo '[0-9]{1,}' >> Results/resultdot$tld.txt
+        done < "TLDSep/dot$tld.txt"
+    done
+    echo -e "\n" >> Results/resultdot$tld.txt
+done    
