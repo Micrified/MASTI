6363359718486146d790d700e4e2210dd8b626fe
Pawel Mack <paw.mackowiak@gmail.com>
2019-05-31 22:40:06 +0200

    The monday state of affairs.

--- DoH/doh.sh
+++ DoH/doh.sh
@@ -29,3 +29,10 @@ do
     done
     echo -e "\n" >> Results/resultdot$tld.txt
 done    
+
+for i in `seq 1 5`;
+do
+## this puts all the searched domains from the inputted domain list
+tld=$(sed "${i}q;d" ../tld.txt)
+echo -e "Resolver:" >> Results/resultdot$tld.txt
+done
