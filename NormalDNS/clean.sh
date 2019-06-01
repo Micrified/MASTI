1a66e40f5590d3e2768661538e860d15364c374d
nikkazah <nikoleta.zaharieva@gmail.com>
2019-06-02 01:31:05 +0200

    adding interactive cleaning script

---
+++ NormalDNS/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Deleting every argument
+# SPECIFYING PATH TO FILE WILL ALSO DELETE IT
+for item in "$@"; do
+    rm -rfv "$(readlink -e "$item")";
+done
+
+#rm -rfv '/home/nika/git/MASTI/NormalDNS/TLDsep';
+#rm -rfv '/home/nika/git/MASTI/NormalDNS/Results';
