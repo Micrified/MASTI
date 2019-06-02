dc425ef9c82ebfbb9405d26c7391651d7c9323b6
nikkazah <nikoleta.zaharieva@gmail.com>
2019-06-02 19:45:22 +0200

    loading domains from files in folder

--- NormalDNS/dns_resolver.sh
+++ NormalDNS/dns_resolver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -A RESOLVERS;
+declare -A QUERIES_FILES;
 
 function load_resolvers()
 {
@@ -18,52 +19,63 @@ function load_resolvers()
     done  < "$(readlink -e "$path")"
 }
 
-function perform_test()
+function load_queries()
 {
-    local queries_path="$(readlink -e "$1")";
+    local queries_dir="$(readlink -e "$1")"; shift 1;
 
-    local ip='';
-    local tld='';
-    local result_line='';
-    local results_dir="$(dirname $(readlink -e "$0"))/Results";
-    local tlds_sep_dir="$(dirname $(readlink -e "$0"))/TLDsep";
-    declare -a measurements=();
-    declare -a tlds=();
+    local query_filepath='';
+    local query_tld='';
 
-    mkdir -pv "$tlds_sep_dir";
-    mkdir -pv "$results_dir";
-    
-    readarray -t tlds < <(
-	grep -Po '(\w+)\.?\s' domainListTestSmall.tsv \
-	    | grep -Po '^[a-zA-z\d]+' \
-	    | sort -u;
-    );
+    for query_filepath in ${queries_dir}/*; do
+	query_tld="$(
+            basename "$query_filepath" \
+                | grep -Po '(?<=^dot).*?(?=\.)';
+        )";
 
-    # echo "DNS resolver,Min(ms,)Average(ms),Max(ms)";
-    for tld in "${tlds[@]}"; do
-	grep -P "\b${tld}\b\.\s" "$queries_path" >> "${tlds_sep_dir}/dot${tld}.txt";
-    done
-
-    for tld in "${tlds[@]}"; do
-	for ip in "${!RESOLVERS[@]}"; do
-	    result_line="$(
-		dnsperf -d "${tlds_sep_dir}/dot${tld}.txt" -s "$ip" | grep -i 'average latency';
-            )";	   
-            readarray -t measurements < <(
-		echo "$result_line" | grep -Po '[\.\d]+';
-            );
-
-            measurements[0]="$(echo "${measurements[0]} * 1000" | bc)";
-            measurements[1]="$(echo "${measurements[1]} * 1000" | bc)";
-            measurements[2]="$(echo "${measurements[2]} * 1000" | bc)";
-
-            echo "${RESOLVERS[$ip]},${measurements[1]},${measurements[0]},${measurements[2]}" >> "${results_dir}/resultdot${tld}.txt";
-	done
+	if [ -n "$query_tld" ];then
+	    QUERIES_FILES["$query_tld"]="$query_filepath";
+	fi
     done
 }
 
+function perform_test()
+{
+    local tld="$1";        shift 1;
+    local query_path="$1"; shift 1;
+
+    local ip='';
+    local result_line='';
+    local results_dir="$(dirname $(readlink -e "$0"))/Results";
+    declare -a measurements=();
+
+    mkdir -pv "$results_dir";
+
+    for ip in "${!RESOLVERS[@]}"; do
+    	result_line="$(
+    	    dnsperf -d "$query_path" -s "$ip" | grep -i 'average latency';
+    	)";
+    	readarray -t measurements < <(
+    	    echo "$result_line" | grep -Po '[\.\d]+';
+    	);
+
+    	measurements[0]="$(echo "${measurements[0]} * 1000" | bc)";
+    	measurements[1]="$(echo "${measurements[1]} * 1000" | bc)";
+    	measurements[2]="$(echo "${measurements[2]} * 1000" | bc)";
+
+    	echo "${RESOLVERS[$ip]},${measurements[1]},${measurements[0]},${measurements[2]}" >> "${results_dir}/resultdot${tld}.txt";
+    done
+}
+
+
+
+#################-| MAIN |-#################
+
 RESOLVERS_PATH="$1"; shift 1;
-QUERIES_PATH="$1";   shift 1;
+QUERIES_DIR="$1";    shift 1;
 
 load_resolvers "$RESOLVERS_PATH";
-perform_test   "$QUERIES_PATH";
+load_queries   "$QUERIES_DIR";
+
+for TLD in "${!QUERIES_FILES[@]}"; do
+    perform_test "$TLD" "${QUERIES_FILES[$TLD]}";
+done
