816a724997099a66b75eef2aee92e238ad1349ff
Barnabas Busa <bbusa@wlan-145-94-155-81.wlan.tudelft.nl>
2019-06-03 13:28:20 +0200

    update DNNSEC

--- DNSSEC/dnssec.sh
+++ DNSSEC/dnssec.sh
@@ -12,10 +12,10 @@ done
 
 
 ## This funciton gets the IPs from the dns_resolvers
-while read line; do
-  ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$line")"
-  echo "$ip" >> ../dns_resolversIP.txt
-done < "../dns_resolvers.txt"
+# while read line; do
+#   ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$line")"
+#   echo "$ip" >> ../dns_resolversIP.txt
+# done < "../dns_resolvers.txt"
 
 ## Here you can select how many of the selected resolvers you want to be using
 for k in `seq 1 2`;
@@ -32,7 +32,7 @@ do
         tld=$(sed "${j}q;d" ../tld.txt)
         while read line; do 
             dig @$resolver $line + dnssec +multi  | pcregrep -M "flags:(.*)ad(.|\n)*Query time:.*" | grep time | grep -Eo '[0-9]{1,}' >> Results/resultdot$tld.txt
-        done < "../LiveDomains/sdot$tld.txt"
+        done < "../LiveDomains/dot$tld.txt"
     done
     echo -e "\n" >> Results/resultdot$tld.txt
 done    
