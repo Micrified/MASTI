167530b464aee1109afb434f7b8d4ba3a6f695f6
joep <joep@dumont.nl>
2019-06-03 13:40:44 +0200

    Made it use a folder of queryfiles

--- DoT/DoT.sh
+++ DoT/DoT.sh
@@ -20,16 +20,37 @@ function load_resolvers()
     done  < "$(readlink -e "$path")"
 }
 
+function load_queries()
+{
+    local queries_dir="$(readlink -e "$1")"; shift 1;
+
+    local query_filepath='';
+    local query_tld='';
+
+    for query_filepath in ${queries_dir}/*; do
+    query_tld="$(
+            basename "$query_filepath" \
+                | grep -Po '(?<=^dot).*?(?=\.)';
+        )";
+
+    if [ -n "$query_tld" ];then
+        QUERIES_FILES["$query_tld"]="$query_filepath";
+    fi
+    done
+}
+
+
 function perform_test()
 {
-    local queries_path="$(readlink -e "$1")";
+    local tld="$1";        shift 1;
+    local query_path="$1"; shift 1;
 
     local ip='';
     local result_line='';
     declare -a measurements=();
     echo "DNS resolver,Min(ms),Average(ms),Max(ms)";
     for ip in "${!RESOLVERS[@]}"; do
-        result=$(./flame -f $queries_path -P tcptls -p 853 "$ip" -n 1 -c 1 -Q 100 | grep "min/avg/max"  | grep -Po -m1 "[\d+\.\d+/?]+ms" | sed 's/ms//' | sed 's/\//,/g')
+        result=$(./flame -f $query_path -P tcptls -p 853 "$ip" -n 1 -c 1 -Q 100 | grep "min/avg/max"  | grep -Po -m1 "[\d+\.\d+/?]+ms" | sed 's/ms//' | sed 's/\//,/g')
         echo "${RESOLVERS[$ip]},$result"
     done
 }
@@ -40,4 +61,8 @@ QUERIES_PATH="$1";   shift 1;
 mkdir -p Results
 
 load_resolvers "$RESOLVERS_PATH";
-perform_test "$QUERIES_PATH";
+load_queries   "$QUERIES_DIR";
+
+for TLD in "${!QUERIES_FILES[@]}"; do
+    perform_test "$TLD" "${QUERIES_FILES[$TLD]}";
+done
