1d3dfb25555f769ce8ad27c9d6a07d556a7ac772
Barnabas Busa <bbusa@wlan-145-94-155-81.wlan.tudelft.nl>
2019-06-03 11:26:07 +0200

    Modified the clean scripts so that they wouldn't fuck shit up

--- NormalDNS/clean.sh
+++ NormalDNS/clean.sh
@@ -2,9 +2,12 @@
 
 # Deleting every argument
 # SPECIFYING PATH TO FILE WILL ALSO DELETE IT
-for item in "$@"; do
-    rm -rfv "$(readlink -e "$item")";
-done
+#for item in "$@"; do
+#    rm -rfv "$(readlink -e "$item")";
+#done
 
 #rm -rfv '/home/nika/git/MASTI/NormalDNS/TLDsep';
 #rm -rfv '/home/nika/git/MASTI/NormalDNS/Results';
+
+cd Results
+rm *.txt
