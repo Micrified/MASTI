0213ca94cd85b9326b6dcf1799d517237bd66740
Barnabas Busa <busa.barnabas@gmail.com>
2019-06-18 17:21:32 +0200

    Delete complete.sh

--- Plot/complete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-url='https://discordapp.com/api/webhooks/535984301984972860/1VDCaTXcuN4e1LNmy7Km4M68ipbnxcqGGSXhQSsgkyprhwCBpgoPdn1rExwoXM3nlLss'
-sleep 5
-message="Your shit is done, go check on it."
-msg_content=\"$message\"
-curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
